ec3eed30e79a9f8ef2efee0f34d77a861616f580
lizhuohui <307317684@qq.com>
2017-09-16 00:06:12 +0800

    backup for clean

---
+++ debug (复件).cmd
@@ -0,0 +1,10 @@
+target remote localhost:1234
+directory ./
+set architecture i8086
+set disassembly-flavor intel
+
+
+
+
+
+
--- debug.cmd
+++ debug.cmd
@@ -2,7 +2,7 @@ target remote localhost:1234
 directory ./
 set architecture i8086
 set disassembly-flavor intel
-b *0x7c00 
-c
+
+
 
 
